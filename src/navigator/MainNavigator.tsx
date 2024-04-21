a08bb7dafe88187ea256c84bacf7d9895f59b617
Nikola Lukic <iamnikolalukic@gmail.com>
2024-04-21 21:26:23 +0200

    Initial components, styles, colors

--- src/navigator/MainNavigator.tsx
+++ src/navigator/MainNavigator.tsx
@@ -1,12 +1,32 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabsNavigator from "@navigator/TabsNavigator";
+import Exercises from "@screens/exercises/Exercises";
+import colors from "theme/colors";
 
 const Stack = createNativeStackNavigator();
 
 function MainNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="Tabs" component={TabsNavigator} />
+      <Stack.Screen
+        name="Exercises"
+        component={Exercises}
+        options={{
+          presentation: "modal",
+          headerShown: true,
+          headerTitleStyle: {
+            color: colors.white,
+          },
+          headerStyle: {
+            backgroundColor: colors.secondary,
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#04011c",
+      secondary: "#0b0f24",
+      ternary: "#c8e2e5",
+      accent: "#9e030d",
+
+      //
+      white: "#ffffff",
+      black: "#000000",
+    },
   },
   plugins: [],
 };
