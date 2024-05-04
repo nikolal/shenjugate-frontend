3bb1a2a0efb83a4c97ad800b8f9a25de3948213a
Nikola Lukic <iamnikolalukic@gmail.com>
2024-05-05 00:20:56 +0200

    Update workout UI

--- src/components/WorkoutItem.tsx
+++ src/components/WorkoutItem.tsx
@@ -2,34 +2,104 @@ import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import colors from "theme/colors";
-import { Exercise, Force, Mechanic, PrimaryMuscles } from "types/exercise";
+import { Exercise } from "types/exercise";
 
 type WorkoutItemProps = {
   onPress: () => void;
   exercise: Exercise;
+  index: number;
 };
 
-function WorkoutItem({ onPress, exercise }: WorkoutItemProps) {
+function ExerciseTable() {
+  return <View></View>;
+}
+
+function ExerciseType({ index }: { index: number }) {
+  if (index === 0) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="flash" size={20} color={colors.accentBlue} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Speed</Text>
+      </View>
+    );
+  } else if (index === 1) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="barbell" size={20} color={colors.accentRed} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Strength</Text>
+      </View>
+    );
+  } else if (index === 2) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="arrow-redo" size={20} color={colors.accentGreen} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Repetition</Text>
+      </View>
+    );
+  } else if (index === 3) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="hammer" size={20} color={colors.accentPurple} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Assistance</Text>
+      </View>
+    );
+  } else if (index === 4) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="medkit" size={20} color={colors.accentPink} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Weakness</Text>
+      </View>
+    );
+  } else if (index === 5) {
+    return (
+      <View className="flex-row">
+        <Ionicons name="hourglass" size={20} color={colors.accentYellow} />
+        <Text className="ml-2 mb-2 text-sm text-textSecondary">Endurance</Text>
+      </View>
+    );
+  }
+}
+
+function ExerciseData() {
+  return (
+    <View className="h-min bg-secondary flex-column border-[0.5px] p-1 border-ternary rounded-lg">
+      <View className="w-full flex-row">
+        <Text className="flex-1 text-center text-sm text-textSecondary">%</Text>
+        <Text className="flex-1 text-center text-sm text-textSecondary">
+          Reps
+        </Text>
+        <Text className="flex-1 text-center text-sm text-textSecondary">
+          Sets
+        </Text>
+        <Text className="flex-1 text-center text-sm text-textSecondary">
+          Weight
+        </Text>
+      </View>
+
+      <View className="h-min flex-row border-t-[0.5px] border-ternary ">
+        <Text className="flex-1 text-center text-sm text-textPrimary">50%</Text>
+        <Text className="flex-1 text-center text-sm text-textPrimary">5</Text>
+        <Text className="flex-1 text-center text-sm text-textPrimary">1</Text>
+        <Text className="flex-1 text-center text-sm text-textPrimary">40</Text>
+      </View>
+    </View>
+  );
+}
+
+function WorkoutItem({ onPress, exercise, index }: WorkoutItemProps) {
   const [weight, setWeight] = useState<string>("");
   return (
-    <View className="flex-row bg-primary border-y-[0.5px] border-ternary mx-2">
-      <TouchableOpacity
-        className="w-9/12 h-16 flex-row items-center pl-4"
-        onPress={onPress}
-      >
-        <Ionicons
-          name="barbell"
-          size={20}
-          color={
-            exercise.mechanic === Mechanic.Compound.toLowerCase()
-              ? colors.accent
-              : colors.accentSecondary
-          }
-        />
-        <Text className="text-md text-white ml-2">{exercise.name}</Text>
+    <View className="flex-row bg-primary border-y-[0.5px] border-ternary mx-1 py-4">
+      <TouchableOpacity className="w-5/12" onPress={onPress}>
+        <ExerciseType index={index} />
+        <Text className="text-base text-white font-semibold">
+          {exercise.name}
+        </Text>
       </TouchableOpacity>
-      <View className="w-3/12 h-16 px-2 py-3">
-        <View className="h-full bg-secondary flex-row border-[0.5px] border-ternary rounded-lg items-center">
+
+      <View className="w-7/12 h-min justify-center items-end">
+        <ExerciseData />
+        {/* <View className="h-full bg-input flex-row border-[0.5px] border-ternary rounded-lg items-center">
           <TextInput
             className="w-8/12 h-full text-white text-center text-md "
             keyboardType="numeric"
@@ -39,8 +109,7 @@ function WorkoutItem({ onPress, exercise }: WorkoutItemProps) {
             autoCorrect={false}
           />
           <Text className="text-md font-bold color-ternary">KG</Text>
-          {/* <Ionicons name="bag-outline" size={20} color={colors.ternary} /> */}
-        </View>
+        </View> */}
       </View>
     </View>
   );
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,14 +1,26 @@
 const colors = {
   primary: "#050505",
-  secondary: "#0A0A0A",
-  ternary: "#484848",
-  accent: "#52EAFF",
-  accentSecondary: "#FA6969",
+  secondary: "#1D1D1D",
+  // ternary: "#484848",
+  ternary: "#5B5B5B",
+  //
+  accentBlue: "#52EAFF",
+  accentRed: "#FA6969",
+  accentGreen: "#91F640",
+  accentYellow: "#FAB63A",
+  accentPurple: "#8E69FB",
+  accentPink: "#FE498A",
   //
   buttonBackground: "#11262A",
   //
   white: "#ffffff",
   black: "#000000",
+  //
+  input: "#0A0A0A",
+  //
+  textPrimary: "#EAEAEA",
+  textSecondary: "#8E8E8E",
+  textTernary: "#7E7E7E",
 };
 
 export default colors;
